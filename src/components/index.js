9b8423e8034f2306791c5cc38f7c9b922ede4a0e
Valerii Shevchenko <46709216+dadaismee@users.noreply.github.com>
2023-10-14 10:36:50 +0200

    Added metric code back

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,6 @@ export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Hero } from './Hero';
 export { default as HowWorks } from './HowWorks';
-export { default as Layout } from './Layout';
 export { default as ListSection } from './ListSection';
 export { default as Pricing } from './Pricing';
 export { default as Program } from './Program';
