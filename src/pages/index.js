70d7af7dddb49a1132ffaa80a941f915998d1536
Valerii Shevchenko <46709216+dadaismee@users.noreply.github.com>
2023-10-14 10:03:18 +0200

    Made metrica script async to avoid interfereing with title

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,13 +61,16 @@ export const Head = () => (
     <SEO />
     <script
       type='text/javascript'
+      async
+      src='https://mc.yandex.ru/metrika/tag.js'
       dangerouslySetInnerHTML={{
         __html: `
           (function (m, e, t, r, i, k, a) {
             m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments); };
             m[i].l = 1 * new Date();
-            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
-          })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1;
+            a.parentNode.insertBefore(k, a);
+          })(window, document, 'script', '', 'ym');
 
           ym(${process.env.YANDEX_METRICA}, 'init', {
             clickmap: true,
