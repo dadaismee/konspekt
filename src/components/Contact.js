4f1cf391d4d436e02b5b052876712ff9d981a72b
Valerii Shevchenko <46709216+dadaismee@users.noreply.github.com>
2024-07-18 11:01:06 +0200

    Added block on co-worlking to process + telegram button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import styled from 'styled-components';
 import { SectionHeading } from '../styles/TextStyles';
-import { Button } from './Hero';
+import { Button, FlexContainer } from './Hero';
 
 const Contact = ({ pageData, id }) => {
   const { title } = pageData;
@@ -31,6 +31,7 @@ const Contact = ({ pageData, id }) => {
         {title}
       </SectionHeading>
 
+      <FlexContainer>
       <ButtonWrapper
         initial={{
           opacity: 0,
@@ -51,11 +52,38 @@ const Contact = ({ pageData, id }) => {
         }}
         viewport={{ once: true }}>
         <a
-          href='mailto:konspekt.school@yandex.ru?subject=Курс «Академическое письмо на стероидах»&body=Добрый день! %0D%0A %0D%0A У меня есть вопрос про курс «Академическое письмо на стероидах»: …'
+          href='mailto:konspekt.school@yandex.ru?subject=Курс «Конспекты, которые пригодятся»&body=Добрый день! %0D%0A %0D%0A У меня есть вопрос про курс «Конспекты, которые пригодятся»: …'
           target='_blank'>
-          <Button>Написать письмо</Button>
+          <Button>Написать на почту</Button>
         </a>
       </ButtonWrapper>
+<ButtonWrapper
+        initial={{
+          opacity: 0,
+          y: 20,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        exit={{
+          opacity: 0,
+          y: 20,
+        }}
+        transition={{
+          ease: [0.165, 0.84, 0.44, 1],
+          duration: 1,
+          delay: 0.3,
+        }}
+        viewport={{ once: true }}>
+        <a
+          href='https://t.me/konspektsupport'
+          target='_blank'>
+          <Button>Написать в Телеграм</Button>
+        </a>
+      </ButtonWrapper>
+
+      </FlexContainer>
     </Wrapper>
   );
 };
@@ -64,4 +92,6 @@ export default Contact;
 
 const Wrapper = styled.section``;
 
-const ButtonWrapper = styled(motion.div)``;
+const ButtonWrapper = styled(motion.div)`
+  width: 100%;
+`;
